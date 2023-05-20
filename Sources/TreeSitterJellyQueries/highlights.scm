a40ac7c3d2988d883ac550ce736c514ca3bfa74d
ActuallyTaylor <zachary.lineman@gmail.com>
2023-05-20 17:18:39 -0400

    Removed Sym links

--- Sources/TreeSitterJellyQueries/highlights.scm
+++ Sources/TreeSitterJellyQueries/highlights.scm
@@ -1,1 +1,30 @@
-queries/highlights.scm
+; keywords
+(import) @keyword
+(number) @number
+(flag (flag_delimeter) @keyword)
+(flag name: (identifier) @keyword)
+
+"func" @keyword.function
+"macro" @keyword.function
+
+[
+  "if"
+  "menu"
+  "return"
+] @keyword
+
+; operators
+(operator) @operator
+
+; functions
+(function_call name: (identifier) @function.call)
+
+; strings
+(string) @string
+(multi_line_string) @string
+
+; comments
+[
+ (comment)
+ (block_comment)
+] @comment @spell
